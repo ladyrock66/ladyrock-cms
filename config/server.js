c219fd1f605e4939fb3ea3151e5dc70939046e4b
Lady Rock <ladyrock66.wd@gmail.com>
2021-05-20 19:17:01 -0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'e09ce73a187a80b877cce1ff34075d0c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f0eaf4dc-65cd-433f-9cdb-5d13ffa07312'
+};
